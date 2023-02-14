e3284e2f97b1870cb8270f6c93a003299462aeb1
Hampson, Shaun R <srh3@hw.ac.uk>
2023-02-14 17:06:57 +0000

    'message'

---
+++ assignment1/assignment1.ino
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define signalA 15
+#define signalB 21
+#define modeButton 23
+#define enableButton 22
+#define mode0 14
+#define mode1 17
+
+void setup() {
+  //variables for pulses
+  int a = 800;  //width of pulses (uS)
+  int b = 100;  //width between pulses (uS)
+  int c = mode0;   //number of pulses in block
+  int d = 5500; //width between pulse blocks
+
+  pinMode(signalA,OUTPUT);
+  pinMode(signalB, OUTPUT);
+  pinMode(modeButton, INPUT);
+  pinMode(enableButton, INPUT);
+}
+
+void loop() {
+  if(enableButton == LOW){
+    if(modeButton == HIGH){
+      c = mode1;
+      } else {
+        c = mode0;
+      }
+    digitalWrite(signalB, HIGH);
+    delay(0.05);
+    digitalWrite(signalB, LOW);
+    for(int i=0; i<c; i++){
+      digitalWrite(signalA, HIGH);
+      delay((a+(0.05*i)));
+      digitalWrite(signalA, LOW);
+      delay(b);
+    }
+    delay(d);
+  }
+
+}
