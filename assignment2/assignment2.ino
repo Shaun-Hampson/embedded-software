1d6b4a040aed0bdbae9c8633450882a7acf15d07
Hampson, Shaun R <srh3@hw.ac.uk>
2023-03-14 17:42:52 +0000

    continued timing tasks

--- assignment2/assignment2.ino
+++ assignment2/assignment2.ino
@@ -1,6 +1,8 @@
 #include <B31DGMonitor.h>
+#include <Ticker.h>
 
 B31DGCyclicExecutiveMonitor monitor;
+Ticker periodicTicker;
 
 #define TASK1PIN 23
 #define TASK2PIN 15
@@ -8,13 +10,13 @@ B31DGCyclicExecutiveMonitor monitor;
 #define TASK4INPUTPIN 34
 #define TASK4OUTPUTPIN 27
 
-float task2;
-float task3;
 int counter;
 float arr[4] = {0, 0, 0, 0};
 float average;
-int maxRange = 0;
+float maxRange = 4095.00;
 int time1, time2, timeDiff;
+float startTime, halfTime, frequency2, frequency3;
+int frequency2Int, frequency3Int;
 
 void setup() {
   Serial.begin(9600);
@@ -30,14 +32,17 @@ void setup() {
 }
 
 void loop() {
+  task2();
+  task3();
   time1 = micros();
-  task2Proceedure();
+  task5();
   time2 = micros();
   timeDiff = time2-time1;
   Serial.println(timeDiff);
+  Serial.println();
 }
 
-void task1Proceedure(){
+void task1(){
   monitor.jobStarted(1);
   //...
   digitalWrite(TASK1PIN, HIGH);
@@ -51,37 +56,84 @@ void task1Proceedure(){
   monitor.jobEnded(1);
 }
 
-void task2Proceedure(){
+void task2(){
   monitor.jobStarted(2);
   //...
-  task2 = pulseIn(TASK2PIN, HIGH);
+  if (digitalRead(TASK2PIN) == HIGH){
+    while(digitalRead(TASK2PIN) == HIGH){
+      //Serial.print("while 1");
+    }
+    startTime = micros();
+    while(digitalRead(TASK2PIN) == LOW){
+      //Serial.print("while 12");
+    }
+    halfTime = micros();
+  }
+  else{
+    while(digitalRead(TASK2PIN) == LOW){
+      //Serial.print("while 2");
+    }
+    startTime = micros();
+    while(digitalRead(TASK2PIN) == HIGH){
+      //Serial.print("while 22");
+    }
+    halfTime = micros();
+  }
+  frequency2 = (float(1)/((halfTime-startTime)*float(2)))*1000000;
+  //Serial.println(halfTime-startTime);
+  //Serial.println(frequency2);
+  frequency2Int = map(frequency2, 333, 1000, 0, 99);
+  frequency2Int = constrain(frequency2Int, 0, 99);
+  Serial.println(frequency2Int);
   monitor.jobEnded(2);
 }
 
-void task3Proceedure(){
+void task3(){
   monitor.jobStarted(3);
   //..
-  task3 = digitalRead(TASK3PIN);
+  if (digitalRead(TASK3PIN) == HIGH){
+    while(digitalRead(TASK3PIN) == HIGH){
+      //Serial.print("while 1");
+    }
+    startTime = micros();
+    while(digitalRead(TASK3PIN) == LOW){
+      //Serial.print("while 12");
+    }
+    halfTime = micros();
+  }
+  else{
+    while(digitalRead(TASK3PIN) == LOW){
+      //Serial.print("while 2");
+    }
+    startTime = micros();
+    while(digitalRead(TASK3PIN) == HIGH){
+      //Serial.print("while 22");
+    }
+    halfTime = micros();
+  }
+  frequency3 = (float(1)/((halfTime-startTime)*float(2)))*1000000;
+  //Serial.println(halfTime-startTime);
+  //Serial.println(frequency2);
+  frequency3Int = map(frequency3, 500, 1000, 0, 99);
+  frequency3Int = constrain(frequency3Int, 0, 99);
+  Serial.println(frequency3Int);
   monitor.jobEnded(3);
 }
 
-void task4Proceedure(){
+void task4(){
   monitor.jobStarted(4);
   //...
-  float task4 = analogRead(TASK4INPUTPIN);  //Read input
-
+  float task4Value = analogRead(TASK4INPUTPIN);  //Read input
   //save input
   int arrPos = counter % 4;
-  arr[arrPos] = task4;
-
+  arr[arrPos] = task4Value;
   //average last 4 values
   average = 0;
-  for (int i = 0; i < sizeof(arr); i++){
+  for (int i = 0; i < 4; i++){
     average += arr[i];
   }
-  average = average/sizeof(arr);
-
-  if(average > maxRange/2){
+  average = average/4;
+  if(average > (maxRange/2)){
     digitalWrite(TASK4OUTPUTPIN, HIGH);
   } else {
     digitalWrite(TASK4OUTPUTPIN, LOW);
@@ -92,12 +144,10 @@ void task4Proceedure(){
   monitor.jobEnded(4);
 }
 
-void task5Proceedure(){
+void task5(){
   monitor.jobStarted(5);
   //...
-  int task2Int = map(task2, 333, 1000, 0, 99);
-  int task3Int = map(task3, 500, 1000, 0, 99);
-  char string = ("Frequency of signal in task 2: %d, Frequency of signal in task 3 %d", task2Int, task3Int);
-  Serial.println(string);
+  //char string = ("%d,%d", frequency2Int, frequency3Int);
+  Serial.println(frequency2Int +","+ frequency3Int);
   monitor.jobEnded(5);
 }
