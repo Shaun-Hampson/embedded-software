a0a4482f233ebfd9a47c17fecd625f84292a1f2a
Hampson, Shaun R <srh3@hw.ac.uk>
2023-03-15 20:34:21 +0000

    finished testing task run times and made schedule
    schedule does not have any violations

--- assignment2/assignment2.ino
+++ assignment2/assignment2.ino
@@ -10,7 +10,7 @@ Ticker periodicTicker;
 #define TASK4INPUTPIN 34
 #define TASK4OUTPUTPIN 27
 
-int counter;
+int counter, frameCounter;
 float arr[4] = {0, 0, 0, 0};
 float average;
 float maxRange = 4095.00;
@@ -29,17 +29,41 @@ void setup() {
   pinMode(TASK4OUTPUTPIN, OUTPUT);
 
   counter = 0;
+  frameCounter = 0;
+
+  schedule();
+  periodicTicker.attach_ms(4,schedule);
+}
+
+void schedule(){
+  int frame = frameCounter % 50;
+  switch(frame){
+    case 0:case 10:case 20:case 30:case 40:
+      task1();  task3();  task4();  break;
+    case 5:case 15:case 25:case 35:case 45:
+      task1();  task2();  task4();  break;
+    case 1:case 11:case 21:case 31:case 41:
+      task1();  task2();  break;
+    case 6:case 32:
+      task1();  task3();  task5();  break;
+    default:
+      if(frame%2 == 0){
+        task1();  task3();
+      } else {
+        task1();
+      }
+      break;
+  }
+  frameCounter++;
 }
 
 void loop() {
-  task2();
-  task3();
-  time1 = micros();
-  task5();
-  time2 = micros();
-  timeDiff = time2-time1;
-  Serial.println(timeDiff);
-  Serial.println();
+  //time1 = micros();
+  //task3();
+  //time2 = micros();
+  //timeDiff = time2-time1;
+  //Serial.println(timeDiff);
+  //Serial.println();
 }
 
 void task1(){
@@ -79,12 +103,12 @@ void task2(){
     }
     halfTime = micros();
   }
-  frequency2 = (float(1)/((halfTime-startTime)*float(2)))*1000000;
-  //Serial.println(halfTime-startTime);
+  frequency2 = (float(1)/((halfTime-startTime)*float(2)))*float(1000000);
+  Serial.println(halfTime-startTime);
   //Serial.println(frequency2);
   frequency2Int = map(frequency2, 333, 1000, 0, 99);
   frequency2Int = constrain(frequency2Int, 0, 99);
-  Serial.println(frequency2Int);
+  //Serial.println(frequency2Int);
   monitor.jobEnded(2);
 }
 
@@ -116,7 +140,7 @@ void task3(){
   //Serial.println(frequency2);
   frequency3Int = map(frequency3, 500, 1000, 0, 99);
   frequency3Int = constrain(frequency3Int, 0, 99);
-  Serial.println(frequency3Int);
+  //Serial.println(frequency3Int);
   monitor.jobEnded(3);
 }
 
@@ -148,6 +172,9 @@ void task5(){
   monitor.jobStarted(5);
   //...
   //char string = ("%d,%d", frequency2Int, frequency3Int);
-  Serial.println(frequency2Int +","+ frequency3Int);
+  Serial.print(frequency2Int);
+  Serial.print(",");
+  Serial.println(frequency3Int);
+  //Serial.flush();
   monitor.jobEnded(5);
 }
